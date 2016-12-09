daf544ba92145957e956a40d57678908e0273b33
Raven <chromefusion44@gmail.com>
2016-12-09 10:06:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import time
-setTemp = int(input("Set temperature to: "))
+setTemp = int(input("Set temp to: "))
 
 while(1==1):
-	temp = int(input("Current temperature: "))
+	#For testing
+	#temp = int(input("Current temp: "))
+	
+	#Get temperature from from sensor
+	python get_temp.py
+	
+	#Determine wether temp is too high or too low,
+	#and how far off it is
 	correction = setTemp - temp
 	if correction > 0:
-		print("Light is now on")
+		#print("Light is now on")
+		#[Code for energizing relay to turn on light here]
 		time.sleep(5)
 	if correction < 0:
-		print("Light is now off")
+		#print("Light is now off")
+		#[Code for denergizing relay to turn off light here]
 		time.sleep(5)
